1e0d388722941e1c61fad6ed99e6e76e27709b05
Leonardo Araujo <mendes.lnr@gmail.com>
2022-06-28 18:58:09 -0300

    Updating test description

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   const { getByText } = render(<App />);
   const title = getByText(/TJFácil/i);
   expect(title).toBeInTheDocument();
